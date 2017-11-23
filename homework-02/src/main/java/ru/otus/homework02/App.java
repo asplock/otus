f5a9274d0f419a4b73ab10b516dd974779ff365d
panfilov.a <panfilov.a@rusgis.com>
2017-11-24 00:58:57 +0500

    ДЗ02:  Добавлено измерение размера контейнера при увеличении его размера

--- homework-02/src/main/java/ru/otus/homework02/App.java
+++ homework-02/src/main/java/ru/otus/homework02/App.java
@@ -1,8 +1,11 @@
 package ru.otus.homework02;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +17,18 @@ public class App {
 
   private static final String PATTERN = "{} size is {} bytes";
 
+  private static final String COLLECTION_PATTERN = "{} size changes: {}";
+
   public static void main(String... args) {
     MemoryCounter counter = new MemoryCounter();
+    countMemoryForObjects(counter);
+    countMemoryForCollectionsSize(counter);
+  }
+
+  private static void countMemoryForObjects(MemoryCounter counter) {
+    log.info("========= OBJECTS SIZES ===========");
     BiConsumer<String, Supplier<?>> printMemory = (name, supplier) ->
-        log.info(PATTERN, name, counter.countMemory(supplier, ITEM_COUNT));
+        log.info(PATTERN, name, counter.countObjectMemory(supplier, ITEM_COUNT));
 
     printMemory.accept("EMPTY STRING", String::new);
     printMemory.accept("EMPTY OBJECT", Object::new);
@@ -30,6 +41,18 @@ public class App {
     printMemory.accept("EMPTY LIST", ArrayList::new);
     printMemory.accept("EMPTY MAP", HashMap::new);
     printMemory.accept("EMPTY SET", HashSet::new);
+  }
+
+  private static void countMemoryForCollectionsSize(MemoryCounter counter) {
+    log.info("========= COLLECTIONS SIZE CHANGES ===========");
+    BiConsumer<String, Supplier<Collection<TestObject>>> printMemory = (name, collectionSupplier) ->
+        log.info(COLLECTION_PATTERN, name,
+                 counter.countContainerMemoryChanges(TestObject::new, collectionSupplier));
+
+    printMemory.accept("ARRAY_LIST", ArrayList::new);
+    printMemory.accept("LINKED_LIST", LinkedList::new);
+    printMemory.accept("HASH_SET", HashSet::new);
+    printMemory.accept("LINKED_HASH_SET", LinkedHashSet::new);
   }
 
 }
--- homework-02/src/main/java/ru/otus/homework02/MemoryCounter.java
+++ homework-02/src/main/java/ru/otus/homework02/MemoryCounter.java
@@ -1,9 +1,15 @@
 package ru.otus.homework02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -12,21 +18,36 @@ public class MemoryCounter {
   private static LongSupplier ALLOCATED_MEMORY = () ->
       Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
-  public <T> long countMemory(Supplier<T> supplier, int itemCount) {
+  public <T> long countObjectMemory(Supplier<T> supplier, int itemCount) {
     List<T> list = new ArrayList<>();
-    Runtime.getRuntime().gc();
+    long memory = fillAndCount(supplier, list, itemCount);
+    return memory / list.size();
+  }
 
+  public <T> Map<Integer, Long> countContainerMemoryChanges(Supplier<T> objectSupplier,
+                                                            Supplier<Collection<T>> collectionSupplier) {
+    long objectMemory = countObjectMemory(objectSupplier, 100_000);
+    Map<Integer, Long> result = new LinkedHashMap<>();
+    Stream.of(10, 100, 1_000, 10_000, 100_000).forEach(size -> {
+      Collection<T> collection = collectionSupplier.get();
+      long fullMemory = fillAndCount(objectSupplier, collection, size);
+      result.put(collection.size(), fullMemory - collection.size() * objectMemory);
+    });
+    return result;
+  }
+
+  private <T> long fillAndCount(Supplier<T> supplier, Collection<T> collection, int itemCount) {
+    Runtime.getRuntime().gc();
     long before = ALLOCATED_MEMORY.getAsLong();
-    log.info("Allocated memory before gc {} bytes", before);
+    log.debug("Allocated memory before gc {} bytes", before);
     for (int i = 0; i < itemCount; i++) {
-      list.add(supplier.get());
+      collection.add(supplier.get());
     }
     Runtime.getRuntime().gc();
 
     long after = ALLOCATED_MEMORY.getAsLong();
-    log.info("Allocated memory after gc {} bytes", after);
-
-    return (after - before) / list.size();
+    log.debug("Allocated memory after gc {} bytes", after);
+    return after - before;
   }
 
 }
